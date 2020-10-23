3a17f7ceb68128372a0267d84df102e0932b4631
unruffled-happiness <andersonk@mbc.qld.edu.au>
2020-10-23 10:20:29 +1000

    chet memont

--- aw man.py
+++ aw man.py
@@ -1,11 +1,12 @@
 from sense_hat import SenseHat
-
+from time import sleep
 sense = SenseHat()
 sense.clear()
 
 # define colours
-B = (0,0,0)
-R = (255,0,0)
+b = (0,0,255)
+B = (102,51,0)
+S = (205,133,63)
 W = (25,255,255)
 
 # setup matrix variable
@@ -14,10 +15,13 @@ image_pixels = [
     B,B,B,B,B,B,B,B,
     B,S,S,S,S,S,S,B,
     S,S,S,S,S,S,S,S,
-    S,W,b,S,S,b,W,S,
+    S,b,W,S,S,b,W,S,
     S,S,S,B,B,S,S,S,
     S,S,B,S,S,B,S,S,
     S,S,B,B,B,B,S,S
     ]
 
 sense.set_pixels(image_pixels)
+while True:
+    sleep(1)
+    sense.flip_h()
